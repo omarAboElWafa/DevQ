a2b78d987531408d518eebb9b5dc4386e9277a3b
omarabdelaziz9395@gmail.com <omarabdelaziz9395@gmail.com>
2020-04-08 10:09:23 +0200

    Express and MongoDB setup

---
+++ config/db.js
@@ -0,0 +1,23 @@
+//bringing needed packages to connect to Database
+const mongoose = require("mongoose");
+const config = require("config");
+
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    //await as "mongoose.connect return promise"
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+
+    //Exist process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-//import express
+//import dependencies
 const express = require("express");
+const connectDB = require("./config/db");
 
 //intialize app variable
 const app = express();
-app.get("/", (req, res) => res.send("API Running"));
 
+//connect Database
+connectDB();
+
+//app.get("/", (req, res) => res.send("API Running"));
+
+//Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 //this will look for environment variable wehn deployment to Heroku
-const PORT = process.env.PORT || 500;
 
+const PORT = process.env.PORT || 500;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
